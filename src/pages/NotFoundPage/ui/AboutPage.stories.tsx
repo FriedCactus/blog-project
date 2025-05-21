c8a2fca35edc515c68c88e0e493486b9bac0d955
Aleksey Belyanchikov <friedlemon73@gmail.com>
2025-05-21 11:52:59 +0400

    Added pages stories

---
+++ src/pages/NotFoundPage/ui/AboutPage.stories.tsx
@@ -0,0 +1,19 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {NotFoundPage} from "./NotFoundPage";
+import {ThemeDecorator} from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import {Theme} from "app/providers/ThemeProvider";
+
+const meta: Meta<typeof NotFoundPage> = {
+    title: 'pages/NotFoundPage',
+    component: NotFoundPage,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof NotFoundPage>;
+
+export const Light: Story = {};
+
+export const Dark: Story = {
+    decorators: [ThemeDecorator(Theme.DARK)]
+};
