cc68367e712cd4ff83fac6084aee603ea1046120
Aleksey Belyanchikov <friedlemon73@gmail.com>
2025-05-21 17:40:14 +0400

    Fix webpack production build

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,20 +4,27 @@ import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 
-export const buildPlugins = ({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] => ([
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({
-        template: paths.html,
-    }),
-    new MiniCssExtractPlugin({
-        filename: "css/[name].[contenthash:8].css",
-        chunkFilename: "css/[name].[contenthash:8].css",
-    }),
-    new webpack.DefinePlugin({
-        __IS_DEV__: isDev
-    }),
-    new BundleAnalyzerPlugin({
-        openAnalyzer: false
-    })
-]);
+export const buildPlugins = ({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] => {
+    const plugins = [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: paths.html,
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[contenthash:8].css",
+            chunkFilename: "css/[name].[contenthash:8].css",
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: isDev
+        }),
+    ];
+
+    if (isDev) plugins.push(
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        })
+    );
+
+    return plugins;
+};
 
