4df1dd92b15584a7ee56f37c27beb234e6e9acf5
Aleksey Belyanchikov <friedlemon73@gmail.com>
2025-06-16 18:00:55 +0400

    Fixed request headers

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,8 +3,10 @@ import {USER_LOCALSTORAGE_KEY} from "../const";
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        // Имитация считывания токена
-        Authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY)
-    }
+});
+
+$api.interceptors.request.use((config) => {
+    config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? '';
+
+    return config;
 });
